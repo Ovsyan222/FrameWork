9df7ae2533bef46f5c4cba794aa06236afd9cab7
Ovsyan222 <ovsyansuper228@gmail.com>
2025-11-19 13:41:29 +0300

    View

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -2,15 +2,30 @@
 
     namespace MyProject\Controllers;
 
+    use MyProject\View\View;
+
     class MainController
     {
+        private $view;
+
+        public function __construct()
+        {
+            $this->view = new View(__DIR__ . '/../../../templates');
+        }
+
         public function main()
         {
-            echo 'Главная страница';
+            $articles = [
+                ['name' => 'Cтатья 1', 'text' => 'Текст статьи 1'],
+                ['name' => 'Cтатья 2', 'text' => 'Текст статьи 2'],
+                ['name' => 'Cтатья 3', 'text' => 'Текст статьи 3'],
+            ];
+
+            $this->view->renderHtml('main/main.php', ['articles' => $articles]);
         }
 
         public function sayHello(string $name)
         {
-            echo 'Привет, ' .$name;
+            $this->view->renderHtml('main/hello.php', ['name' => $name]);
         }
     }
